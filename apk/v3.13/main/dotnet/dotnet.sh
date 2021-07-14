2f099a9745ac46512aee873068ac360f2c918198
build@apk-groulx <build@apk-groulx.praxis>
2021-07-14 03:56:39 +0000

    update

--- apk/v3.13/main/dotnet/dotnet.sh
+++ apk/v3.13/main/dotnet/dotnet.sh
@@ -10,3 +10,4 @@ esac
 
 # Extract self-contained executables under HOME to avoid multi-user issues from using the default '/var/tmp'
 [ -z "$DOTNET_BUNDLE_EXTRACT_BASE_DIR" ] && export DOTNET_BUNDLE_EXTRACT_BASE_DIR="${XDG_CACHE_HOME:-"$HOME"/.cache}/dotnet_bundle_extract"
+
